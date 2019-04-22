ca97be3764fe3ac07196831d26a736e4dbad01af
tarinasari09 <tarinasari0913@gmail.com>
2019-04-22 19:53:09 +0700

    tokap23

--- resources/views/admin/pages/kategori/add.blade.php
+++ resources/views/admin/pages/kategori/add.blade.php
@@ -16,10 +16,13 @@
 				<div class="card-body">
 					<div class="form-group form-label-group">
 						<input type="text" name="Kategori"
-						class="form-control"
+						class="form-control {{ $errors->has('kategori')?'is-invalid':'' }}"
 						value="{{ old('kategori') }}"
 						id="iKategori" placeholder="Kategori" required>
 						<label for=iKategori>Kategori</label>
+						@if( $errors->has('kategori') )
+						<div class="invalid-feedback">{{ $errors->first('kategori') }}</div>
+						@endif
 
 					</div><!-- End Form Group -->
 
